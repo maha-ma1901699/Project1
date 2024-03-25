9c942f1ea97448db2292d5b75b12ff7c768e219b
deema-de1800835 <158223669+deema-de1800835@users.noreply.github.com>
2024-03-25 23:17:42 +0300

    handlesearch function

--- phase1/js/search.js
+++ phase1/js/search.js
@@ -3,6 +3,9 @@
 addEventListener("load", handleLoad);
 async function handleLoad(){ 
     const cardsContainer = document.querySelector("#cards"); 
+    const search_button = document.querySelector("#search_button"); 
+   search_button. addEventListener("click", handleSearch);
+    
 
     // alert("hello") 
     let url= "data/products.json"
@@ -19,6 +22,25 @@ async function handleLoad(){
     }
     // cardsContainer.innerHTML = html
     // alert(html)
+}
+async function handleSearch(){
+    const cardsContainer = document.querySelector("#cards"); 
+    const searchText = document.querySelector("#search"); 
+    let url= "data/products.json"
+    const data = await fetch(url)
+    const productList = await data.json()
+if(searchText.value === "") {
+    let html = productListToCards(productList)
+    cardsContainer.innerHTML = html
+
+
+}
+else{
+    const filteredproduct = productList.filter(p=>p.productName.includes(searchText.value))
+    
+    let html = productListToCards(filteredproduct)
+    cardsContainer.innerHTML = html
+}
 }
 
 function productToCard(product){
