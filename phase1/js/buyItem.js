af3b93fbe88581bd1985170838d7f187131d64d7
maha-ma1901699 <ma1901699@qu.edu.qa>
2024-03-28 16:43:55 +0300

    total price updated auto

--- phase1/js/buyItem.js
+++ phase1/js/buyItem.js
@@ -5,11 +5,13 @@ async function handlePageLoad(){
     const itemnamespan= document.querySelector("#itemname")
     const itempricespan= document.querySelector("#itemprice")
     const itemimage= document.querySelector("#productimage")
-
-
+    const userinfodiv = document.querySelector("#user-info"); 
+    const quantityinput = document.querySelector("#quantity-input"); 
+    quantityinput.addEventListener("change", handleQuantityChange)
     const userinformation = localStorage.getItem("userobject")
     if(userinformation){
         const customer = JSON.parse(userinformation)
+        userinfodiv.innerHTML=`${customer.name} ${customer.surename}`
         balancespan.innerHTML=customer.moneyBalance
         let url= "data/products.json"
         const data = await fetch(url)
@@ -30,6 +32,15 @@ async function handlePageLoad(){
     window.location.href= "login.html"
 }
 
+
 }
 
-    
+  function handleQuantityChange(e){
+    const quantity= parseInt(e.target.value)
+    const itempricespan= document.querySelector("#itemprice")
+    const productPrice=parseInt(itempricespan.innerHTML)
+    const total= quantity*productPrice
+    const totalinput= document.querySelector("#total")
+    totalinput.value=total
+
+  }  
