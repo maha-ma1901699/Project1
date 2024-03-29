05bfa7852c0306582a9721537595ec9e73595acd
deema-de1800835 <158223669+deema-de1800835@users.noreply.github.com>
2024-03-30 00:29:28 +0300

    added current user and moved login page logic to repo

--- phase1/js/Repository.js
+++ phase1/js/Repository.js
@@ -1,24 +1,24 @@
- class Repository {
+class Repository {
     constructor() {
         this.userspath = "data/users.json"
         this.customerspath = "data/customers.json"
         this.sellerpath = "data/seller.json"
         this.productspath = "data/products.json"
-        this.initialize ()
+        this.initialize()
     }
-    async initialize(){
-        const allpaths= [
-            this.userspath , this.customerspath , this.sellerpath , this.productspath
+    async initialize() {
+        const allpaths = [
+            this.userspath, this.customerspath, this.sellerpath, this.productspath
 
         ]
-        if(!localStorage.getItem("data_loaded")) {
+        if (!localStorage.getItem("data_loaded")) {
             for (const path of allpaths) {
-                const data = await fetch(path) 
+                const data = await fetch(path)
                 const objects = await data.json()
-                localStorage.setItem(path,JSON.stringify(objects))
-                
+                localStorage.setItem(path, JSON.stringify(objects))
+
             }
-            localStorage.setItem("data_loaded",true)
+            localStorage.setItem("data_loaded", true)
 
         }
 
@@ -28,10 +28,41 @@
         const data = JSON.parse(localStorage.getItem(this.userspath))
         return data
     }
+    getCustomers() {
+        const data = JSON.parse(localStorage.getItem(this.customerspath))
+        return data
+    }
+    getSellers() {
+        const data = JSON.parse(localStorage.getItem(this.sellerpath))
+        return data
+    }
+    getCurrentUser() {
+        if (localStorage.getItem("userobject"))
+            return JSON.parse(localStorage.getItem("userobject"))
+        else
+            return null
+    }
 
-    login(strUserName,strPassword){
+    login(strUserName, strPassword) {
         const users = this.getUsers()
-        const user= users.find(u => u.username == strUserName && u.password == strPassword)
+        const user = users.find(u => u.username == strUserName && u.password == strPassword)
+        if (user) {
+            if (user.type === "customer") {
+
+                const customers = this.getCustomers()
+                const customer = customers.find(c => c.id === user.id)
+                localStorage.setItem("userobject", JSON.stringify(customer))
+
+
+            }
+            else if (user.type === "customer") {
+
+                const sellers = this.getSellers()
+                const seller = sellers.find(c => c.id === user.id)
+                localStorage.setItem("userobject", JSON.stringify(seller))
+            }
+
+        }
         return user
 
     }
--- phase1/js/login.js
+++ phase1/js/login.js
@@ -17,22 +17,13 @@ async function handleLoginClick() {
             location.href= "admin.html"
 
         }else if (user.type === "customer"){
-            let url= "data/customers.json"
-
-            const data = await fetch(url)
-            const customers = await data.json()
-            const customer = customers.find(c=>c.id === user.id)
-            localStorage.setItem("userobject", JSON.stringify(customer))
+            
 
 
             location.href= "search.html"
         }
         else if (user.type === "seller"){
-            let url= "data/sellers.json"
-            const data = await fetch(url)
-            const sellers = await data.json()
-            const seller = sellers.find(c=>c.id === user.id)
-            localStorage.setItem("sellerobject", JSON.stringify(seller))
+           
             location.href= "sales.html"
         } else{
             alert ("User type not known")
